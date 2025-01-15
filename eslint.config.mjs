ae7858869e269dec8d6e5208f0eefaa060f26675
Bukhtyar Haider Khan <bukhtyar.haider1@gmail.com>
2025-01-15 12:36:28 +0500

    chore: configure ESLint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,56 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: { root: true },
+  ignorePatterns: ["components/ui/**"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-undef": "off",
+      },
+    },
+  ],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+          "object",
+        ],
+        newlinesBetween: "always",
+        pathGroups: [
+          {
+            pattern: "@app/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: false,
+        },
+      },
+    ],
+  },
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  ...compat.plugins("import", "promise", "n"),
 ];
 
 export default eslintConfig;
