c758b21bdb483e7cd26f50e2e54488071e5962de
Bukhtyar Haider Khan <bukhtyar.haider1@gmail.com>
2025-01-27 17:40:18 +0500

    feat(auth): create and implement AuthForm component

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
 import React from "react";
 
 const SignIn = () => {
-  return <></>;
+  return (
+    <AuthForm
+      type="SIGN_IN"
+      schema={SignInSchema}
+      defaulValues={{ email: "", password: "" }}
+      onSubmit={(values) => Promise.resolve({ success: true, values })}
+    />
+  );
 };
 
 export default SignIn;
